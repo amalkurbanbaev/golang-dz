523b62ce2cda1cf3f6bffe1da036be752f78c6f1
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2026-04-06 02:55:47 +0500

    input validation, repeated error input

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,38 +1,116 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+// 1. Сделать меню с шагами
+
+// 3. После получения всех данных с помощью if / switch вычислить итог и вывести результат.
 
 const USD_TO_EUR = 0.94 // 1 USD = 0.94 EUR
 const USD_TO_RUB = 100  // 1 USD = 90 RUB
-const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
+const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR // 106
+const EUR_TO_USD = 1 / USD_TO_EUR
 
-func convert(amount float64, currency1 string, currency2 string) float64 {
-	// if currency1 == "USD" && currency2 == "EUR" {
-	// 	return USD_TO_EUR / amount
-	// }
+func convert(currency1 string, amount float64, currency2 string) float64 {
+	switch {
+	case currency1 == "USD" && currency2 == "EUR":
+		return amount * USD_TO_EUR
+	case currency1 == "USD" && currency2 == "RUB":
+		return amount * USD_TO_RUB
+	case currency1 == "EUR" && currency2 == "USD":
+		return amount * EUR_TO_USD
+	case currency1 == "EUR" && currency2 == "RUB":
+		return amount * EUR_TO_RUB
+	case currency1 == "RUB" && currency2 == "USD":
+		return amount / USD_TO_RUB
+	case currency1 =="RUB" &&currency2 =="EUR":
+		return amount / EUR_TO_RUB
+	}
 
-	// return amount
+	return amount
 }
 
-func handleUserInput() (float64, string, string) {
-	var amount float64
+// 2. Выделить функцию ввода / проверки валюты и числа
+func handleInputCurrency() (string, error) {
+	var currency string
+
+	fmt.Scan(&currency)
+
+	if currency != "USD" && currency != "EUR" && currency != "RUB" {
+		return "", errors.New("Такой валюты нет")
+	}
+
+	return currency, nil
+}
+
+// 2. Выделить функцию ввода / проверки валюты и числа
+func handleInputAmount() (float64, error) {
+	var amountInput float64
+
+	fmt.Scan(&amountInput)
+
+	if amountInput <= 0 {
+		return 0, errors.New("Введено некорректное значение")
+	}
+
+	return amountInput, nil
+
+}
+
+func handleUserInput() (string, float64, string) {
 	var currency1 string
+	var amount float64
 	var currency2 string
 
-	fmt.Print("Введите сумму для обмена: ")
-	fmt.Scan(&amount)
+	// Ввод исходной валюты (подсказываем варианты) - если ошибка, заново вводим
 
-	fmt.Print("Какую валюту вы желаете обменять? ")
-	fmt.Scan(&currency1)
+	for true {
+		fmt.Print("Какую валюту вы желаете обменять (USD, EUR, RUB) ? ")
+		currencyInput, error := handleInputCurrency()
 
-	fmt.Print("Какую валюту вы желаете получить? ")
-	fmt.Scan(&currency2)
+		if error != nil {
+			continue
+		} else {
+			currency1 = currencyInput
+			break
+		}
 
-	return amount, currency1, currency2
+	}
+
+	// Ввод числа - если ошибка, заново вводим
+	for true {
+		fmt.Print("Введите сумму обмена: ")
+		amountInput, error := handleInputAmount()
+		if error != nil {
+			continue
+		} else {
+			amount = amountInput
+			break
+		}
+	}
+
+	// Ввод целевой валюты (подсказываем варианты, кроме исходной валюты) - если ошибка, заново вводим
+	for true {
+		fmt.Print("Какую валюту вы желаете получить? ")
+		currencyInput, error := handleInputCurrency()
+
+		if error != nil {
+			continue
+		} else {
+			currency2 = currencyInput
+			break
+		}
+
+	}
+
+	return currency1, amount, currency2
 }
 
 func main() {
-	amount, currency1, currency2 :=  handleUserInput()
+	currency1, amount, currency2 := handleUserInput()
 
-	fmt.Println("Вы получите = ", convert(amount, currency1, currency2))
+	fmt.Println("Вы получите = ", convert(currency1, amount, currency2))
 }
